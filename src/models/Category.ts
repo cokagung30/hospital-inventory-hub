d6387111fd6fe246d04a23ae9b250042ac11aead
cokagung30 <50937866+cokagung30@users.noreply.github.com>
2024-09-14 23:36:09 +0800

    update(category): add schema category on swagger

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,3 +1,36 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Category:
+ *       type: object
+ *       required:
+ *         - id
+ *         - name
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the category
+ *         name:
+ *           type: string
+ *           description: The name of the category
+ *         description:
+ *           type: string
+ *           description: The description of the category
+ *         created_at:
+ *           type: string
+ *           format: date-time
+ *           description: The crated date of the category
+ *         updated_at:
+ *           type: string
+ *           format: date-time
+ *           description: The updated date of the category
+ *       example:
+ *         id: d5fE_asz
+ *         name: ELEKTROMEDIK
+ *         description: Kategori untuk peralatan dengan elektromedik
+ */
+
 export interface ICategory {
     id: string;
     name: string;
