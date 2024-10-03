3684df14d52dacd79b21b9b9ce5a4949d79999e4
cokagung30 <50937866+cokagung30@users.noreply.github.com>
2024-10-04 06:47:07 +0800

    update(elasticsearch): integrate with serverless elasticsearch

--- src/config/elasticsearch.ts
+++ src/config/elasticsearch.ts
@@ -1,8 +1,13 @@
-import { Client } from "@elastic/elasticsearch";
+// import { Client } from "@elastic/elasticsearch";
+
+import { Client } from "@elastic/elasticsearch-serverless";
 
 const esClient = new Client({
     node: process.env.ELASTICSEARCH_NODE || 'http://localhost:9200',
     requestTimeout: 60000,
+    auth: {
+        apiKey: process.env.ELASTICSEARCH_API_KEY || ''
+    }
 });
 
 export {esClient};
